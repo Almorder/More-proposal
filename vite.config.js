b07d7756d51f0329edf939838ad69046f19671eb
Almorder <xalmorder@gmail.com>
2026-06-26 12:23:30 +0200

    Configure Vite and Github Actions for deployment

---
+++ vite.config.js
@@ -0,0 +1,20 @@
+import { defineConfig } from 'vite';
+import { resolve } from 'path';
+
+export default defineConfig({
+  base: '/More-proposal/',
+  build: {
+    rollupOptions: {
+      input: {
+        main: resolve(__dirname, 'index.html'),
+        vision: resolve(__dirname, 'vision.html'),
+        experiences: resolve(__dirname, 'experiences.html'),
+        memory: resolve(__dirname, 'memory.html'),
+        journal: resolve(__dirname, 'journal.html'),
+        destinations: resolve(__dirname, 'destinations.html'),
+        product: resolve(__dirname, 'product.html'),
+        contact: resolve(__dirname, 'contact.html'),
+      }
+    }
+  }
+});
